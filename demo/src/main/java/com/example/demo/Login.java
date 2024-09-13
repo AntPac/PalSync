9bffec8ccf31eadf62b4febcf901bbcfaba8dc27
AntPac <anpacheco100@gmail.com>
2024-09-12 20:50:49 -0700

    Here is an almost complete basic example of the Login It uses both frontend and backend code
    Changes:
    -Created Home(Homepage) method that will show/return the homepage
    -Updated Login to ask for pin then run after submit
    -Created LoginMes which checks for pin (Currently using inside inbuilt array) then responses based off whether or not it is found
    -Updated NewAcc(Signup) to ask for name and pin then run after submit
    -Created NewAccMes which creates new account adds it to storage and responses
    -All pages can now return to home page
    -Created account class to hold user data
    
    This is a barebones code there are no checks for incorrect data and probably still some issues but I made this to show what we can start with
    If you have any questions or want me to explain in further depth just message me

--- demo/src/main/java/com/example/demo/Login.java
+++ demo/src/main/java/com/example/demo/Login.java
@@ -3,7 +3,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
@@ -11,52 +11,138 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 
 public class  Login extends  Application {
-public void start(Stage Stage){
-    Label WelcomeText = new Label("Welcome to PalSync");
-    WelcomeText.setStyle("-fx-font-size: 20pt; ");
-    AnchorPane.setTopAnchor(WelcomeText, 50.0);
+    VBox vbox = new VBox();
+    Image icon = new Image(getClass().getResource("/images/hug.png").toExternalForm());
+    account[] Storage = new account[10];
+    int numofacc = 0;
+    public void start(Stage Stage) {
+        vbox = new VBox(20);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setStyle("-fx-background-color: khaki; ");
 
-    Button LB = new Button("Login");
-    Button SB = new Button("Sign Up");
-
-
-    VBox vbox = new VBox(20);
-    vbox.setAlignment(Pos.CENTER);
-
-    Image icon = new Image (getClass().getResource("/images/hug.png").toExternalForm());
-    Stage.getIcons().add(icon);
+        Stage.getIcons().add(icon);
 
 
-    vbox.getChildren().addAll(WelcomeText,LB,SB);
+        Home();
 
+        Scene scene = new Scene(vbox, 800, 600);
+        Stage.setScene(scene);
+        Stage.setFullScreen(true);
+        Stage.show();
 
-    Scene scene = new Scene(vbox, 800, 600);
+    }
+    public void Home() {
+        vbox.getChildren().clear();
 
-    LB.setOnAction(event -> SignUp(vbox));
-    SB.setOnAction(event -> SignUp(vbox));
+        Label WelcomeText = new Label("Welcome to PalSync");
+        WelcomeText.setStyle("-fx-font-size: 20pt; ");
+        ImageView Image = new ImageView(icon);
+        Button LB = new Button("Login");
+        Button SB = new Button("Are You New Here?");
 
+        LB.setOnAction(event -> login(vbox));
+        SB.setOnAction(event -> NewAcc(vbox));
 
-    Stage.setScene(scene);
-    Stage.setFullScreen(true);
-    Stage.show();
-}
+        vbox.getChildren().addAll(Image, WelcomeText,LB,SB);
+    }
 
+    public void login( VBox box){
 
-    public void SignUp( VBox box){
-    box.getChildren().clear();
-        Label Sname = new Label("Enter a username");
-        TextField Username = new TextField();
-        Label Spass = new Label("Enter a password");
-        TextField Password = new TextField("Enter Password");
-        Button Submit = new Button("submit");
-        box.getChildren().addAll(Sname,Username,Spass,Password,Submit);
+        box.getChildren().clear();
+
+        Label Pint = new Label("Please Enter Your Personal Pin");
+        TextField Pin = new TextField("Ex. 1234");
+
+        Button Submit = new Button("Submit");
+        Button Back = new Button("Back");
+
+        Submit.setOnAction(event -> {
+            int pin = Integer.parseInt(Pin.getText());
+            LoginMes(pin);
+        });
+
+        Back.setOnAction(event -> Home());
+
+        box.getChildren().addAll(Pint,Pin,Submit,Back);
+
 
 
     }
+    public void NewAcc(VBox box) {
+
+        box.getChildren().clear();
+
+        Label namet = new Label("Please Enter A Name");
+        TextField name = new TextField();
+
+        Label Pint = new Label("Please Enter A 4 Digit Pin");
+        TextField Pin = new TextField("Ex. 1234");
+
+        Button Submit = new Button("Submit");
+        Button Back = new Button("Back");
+
+        Submit.setOnAction(event -> {
+            int pin = Integer.parseInt(Pin.getText());
+            NewAccMes(pin, name.getText());
+        });
+        Back.setOnAction(event -> Home());
+
+        box.getChildren().addAll(namet,name,Pint,Pin,Submit,Back);
+
+    }
+
+    public void NewAccMes(int pin, String name){
+        account a = new account(pin,name);
+        Storage[numofacc++]= a;
+
+        vbox.getChildren().clear();
+
+        Label Message = new Label("Welcome To PalSync " + name);
+        Button Back = new Button("Return to Home Page");
+
+        Back.setOnAction(event -> Home());
+        vbox.getChildren().addAll(Message,Back);
+    }
+
+    public void LoginMes(int pin){
+        vbox.getChildren().clear();
+        boolean found = false;
+        String name = "";
+
+        for (int i=0;i< Storage.length;i++){
+            if(Storage[i] != null && Storage[i].accnum==pin){
+                found = true;
+                name = Storage[i].name;
+                break;
+            }
+        }
+
+
+
+        Button Back = new Button("Return to Home Page");
+        Back.setOnAction(event -> Home());
+
+        if (found){
+            Label Acc = new Label("Welcome Back " + name);
+            vbox.getChildren().addAll(Acc,Back);
+        } else {
+            Label noAcc = new Label("Account Doesn't Exist");
+            vbox.getChildren().addAll(noAcc,Back);
+        }
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("javier was here");
+        //System.out.println("javier was here");
         launch();
     }
-
+    static class account{
+        int accnum;
+        String name;
+        public account(int accnum, String name) {
+            this.accnum = accnum;
+            this.name = name;
+        }
+    }
 }
+
