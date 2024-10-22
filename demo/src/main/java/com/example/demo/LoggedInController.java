dc8a036b4ac8022ca48699dd4918785d4bb841dd
Sky Dalangin <134574661+Skaiko@users.noreply.github.com>
2024-10-21 17:12:29 -0700

    Re commiting because it is apparently not updated on everyone elses' end.

--- demo/src/main/java/com/example/demo/LoggedInController.java
+++ demo/src/main/java/com/example/demo/LoggedInController.java
@@ -1,4 +1,54 @@
 package com.example.demo;
 
-public class LoggedInController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+/*
+    IMPORTANT NOTE: READ ME
+
+    This controller is for the page that says "You have logged in".
+    However, we would prefer it that after successful login, we prompt
+    the user to the homePage itself. Implementation on connecting these
+    two will be completed as soon as the loginPage implementation is complete.
+    That will most likely be a separate branch from this one. Or not.
+
+*/
+
+
+
+public class LoggedInController implements Initializable {
+
+
+    @FXML
+    private Button button_logout;
+    @FXML
+    private Label label_welcome;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DBUtils.changeScene(actionEvent, "sample.fxml", "Log in", null);
+            }
+        });
+    }
+
+    public void setUserInformation(String username){
+        label_welcome.setText(STR."Welcome \{username}!");
+    }
+
+
+
 }
+
+
--- demo/src/main/java/com/example/demo/Main.java
+++ demo/src/main/java/com/example/demo/Main.java
@@ -9,15 +9,16 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        primaryStage.setTitle("PS.login");
-        primaryStage.setScene(new Scene(root));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("PalSync");
+        primaryStage.setScene(new Scene(root , 600,400));
         primaryStage.show();
 
     }
 
 
     public static void main(String[] args) {
+        launch(args);
 
     }
 }
